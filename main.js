3a0d806ff87aa40637b88e17e5621931966e5537
Alejandro Prado <adro.viprado@gmail.com>
2025-10-06 16:28:43 -0600

    feat(main): se añade función de verificar ganador. Y se añade arreglo de posibles ganadores

--- main.js
+++ main.js
@@ -5,15 +5,43 @@ const $tabla = document.querySelector(".tabla"),
   $casilla = document.querySelectorAll(".casilla"),
   $botonReinicio = document.querySelector(".btn-reiniciar");
 
-// console.log($casilla);
-// console.log($botonReinicio);
-
 // ===========================================
 // Variables globales de estado
 // ===========================================
 let tablero = Array(9).fill(""),
   turnoJugador = "❌",
   finDelJuego = false;
+
+const COMBINACION_PARA_GANAR = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const verificarGanador = (tablero) => {
+  const hayGanador = COMBINACION_PARA_GANAR.some((conjunto) => {
+    const [a, b, c] = conjunto;
+
+    if (
+      tablero[a] !== "" &&
+      tablero[a] === tablero[b] &&
+      tablero[a] === tablero[c]
+    ) {
+      return true;
+    }
+  });
+
+  if (hayGanador) {
+    finDelJuego = true;
+  }
+
+  return hayGanador;
+};
 
 // ===========================================
 // Delegación de Eventos
@@ -22,26 +50,23 @@ let tablero = Array(9).fill(""),
 // --- "click" (Interacción del Usuario) ---
 document.addEventListener("click", (e) => {
   // --- "click" Tablero de Juego (Interacción del Usuario) ---
-  // console.log(e.target);
   if (e.target.matches(".casilla")) {
-    // console.log(e.target);
     const CASILLA_INDEX = e.target.dataset.value;
-    // console.log(CASILLA_INDEX);
     if (tablero[CASILLA_INDEX] === "") {
       tablero[CASILLA_INDEX] = turnoJugador;
       $casilla[CASILLA_INDEX].textContent = turnoJugador;
-      // turnoJugador = "⭕";
-      console.log(tablero);
 
       if (turnoJugador === "⭕") {
+        verificarGanador(tablero);
         turnoJugador = "❌";
       } else if (turnoJugador === "❌") {
+        verificarGanador(tablero);
         turnoJugador = "⭕";
       }
     } else {
-      return null;
+      return;
     }
   }
-  // --- "click" Botón Reiniciar Marcador (Interacción del Usuario) ---
-  // if (e.target === $botonReinicio) {}
 });
+// --- "click" Botón Reiniciar Marcador (Interacción del Usuario) ---
+// if (e.target === $botonReinicio) {}
